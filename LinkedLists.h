bea7ee7eae4db6c62032ed41b918768b92f5f628
Jacob Tracey <42984369+Jacob-Tr@users.noreply.github.com>
2022-08-10 01:35:54 -0300

    Updates

--- LinkedLists.h
+++ LinkedLists.h
@@ -1,3 +1,5 @@
+// https://github.com/Jacob-Tr/LinkedLists
+
 #ifndef _LINKED_LISTS_H
 #define _LINKED_LISTS_H
 
@@ -15,7 +17,19 @@ enum types
     SHORT,
     UCHAR,
     CHAR
-}
+};
+
+size_t type_size[] =
+{
+    sizeof(unsigned long),
+    sizeof(long),
+    sizeof(unsigned int),
+    sizeof(int),
+    sizeof(unsigned short),
+    sizeof(short),
+    sizeof(unsigned char),
+    sizeof(char)
+};
 
 #define __TYPES__ 8
 
@@ -23,121 +37,168 @@ typedef unsigned short datatype;
 
 struct LinkedListElement
 {
+    struct LinkedListElement* prev;
     void* value;
-    void* next;
-}, NULL_LIST_ELEMENT;
+    unsigned short element_id;
+    struct LinkedListElement* next;
+} NULL_LIST_ELEMENT;
 
 struct LinkedList
 {
     datatype type;
-    size_t size, elements;
+    size_t size;
     
     struct LinkedListElement* first;
-}, NULL_LIST;
+    struct LinkedListElement* last;
+} NULL_LIST;
 
 typedef struct LinkedList linkedlist;
 
+#define LinkedList(T, S) initList((linkedlist*) malloc(sizeof(linkedlist)), (T), (S))
+
 #ifdef __cplusplus
 extern "C" 
+{
 #endif
-linkedlist* initList(linkedlist* list, const datatype type, const size_t size)
+
+linkedlist initList(linkedlist* list, const datatype type, const size_t size)
 {
     if(size < 0 || type > __TYPES__) goto FAILED;
     
     list->type = type;
     list->size = size;
-    list->elements = 0;
     
-    list->first = (LinkedListElement*) malloc(sizeof(struct LinkedListElement));
-    struct LinkedListElement* prev_element = list->first;
+    struct LinkedListElement* element;
+    struct LinkedListElement* prev_element = &NULL_LIST_ELEMENT;
     
-    for(int i = 0; i < size; i++) 
+    for(unsigned short i = 0; i < size; i++) 
     {
-        prev_element->next = (struct LinkedListElement*) malloc(sizeof(struct LinkedListElement));
-        prev_element = prev_element->next;
+        element = (struct LinkedListElement*) malloc(sizeof(struct LinkedListElement));
+        
+        if(!i) list->first = element;
+        
+        element->element_id = i;
+        element->value = malloc(type_size[type]);
+        
+        element->prev = prev_element;
+        prev_element->next = element;
+        prev_element = element;
+        prev_element->value = NULL;
+        
+        element = element->next;
     }
     
-    return list;
+    element = &NULL_LIST_ELEMENT;
+    
+    list->last = prev_element;
+    (list->last)->next = element;
+    
+    return *list;
     
     FAILED:
     free(list);
-    return &NULL_LIST;
+    return NULL_LIST;
 }
 
-#define LinkedList<T>(S) initList((linkedlist*) malloc(sizeof(linkedlist)), T, S)
-
-#ifdef __cplusplus
-extern "C" 
-#endif
-void* get(const linkedlist list, const size_t element)
+struct LinkedListElement* getElement(const linkedlist list, const size_t element)
 {
-    if(element < 0 || element > list->size) return NULL;
-
+    if(element < 0 || element > list.size) return &NULL_LIST_ELEMENT;
+    
     struct LinkedListElement* ret = list.first;
-    for(size_t i = 0; i < element; i++) ret = ret->next;
     
-    return (void*) &ret->value;
+    if(element < (list.size / 2))
+    {
+        ret = list.first;
+        for(size_t i = 0; i < element; i++) ret = ret->next;
+    }
+    else
+    {
+        ret = list.last;
+        for(size_t i = (list.size - 1); i > 0; i--) ret = ret->prev;
+    }
+    
+    return ret;
 }
 
-#ifdef __cplusplus
-extern "C" 
-#endif
-void set(const linkedlist list, const size_t element, const void* value)
+void* get(const linkedlist list, const size_t element)    
 {
-    if(element < 0 || element > list.size) return NULL;
-
-    struct LinkedListElement* el = list.first;
-    for(size_t i = 0; i < element; i++) el = el->next;
+    struct LinkedListElement* ret = getElement(list, element);
     
-    el->value = value;
+    if(ret == &NULL_LIST_ELEMENT) return ret;
+    
+    void* value = ret->value;
+    
+    return value;
 }
 
-#ifdef __cplusplus
-extern "C" 
-#endif
-void add(linkedlist* list, const void* value)
+void set(linkedlist list, const size_t element, void* value)
 {
-    size_t size = list->size;
-    list = realloc(list, (sizeof(linkedlist) * list->size) + 1);
+    struct LinkedListElement* el = getElement(list, element);
     
-    set(*list, (size + 1), value);
+    if(el == NULL) return;
+    
+    switch(list.type)
+    {
+        case UINT:
+        {
+            el->value = ((unsigned int*) value);
+            break;
+        }
+    }
 }
 
-#ifdef __cplusplus
-extern "C" 
-#endif
-void remove(linkedlist* list, const size_t element)
+void add(linkedlist* list, void* value)
 {
-    if(element < 0 || element > list->size) return NULL;
-
-    struct LinkedListElement* el = list.first;
-    size_t index;
+    list = realloc(list, (sizeof(linkedlist) * ++list->size));
     
-    for(index = 0; index < element; index++) el = el->next;
+    struct LinkedListElement* el = getElement(*list, list->size);
+    struct LinkedListElement* prev_el = getElement(*list, (list->size - 1));
     
-    for(size_t i = index; index < (list->size - index) - 1; i++) set(list, i, get(list, (i + 1)));
+    prev_el->next = el;
+    el->prev = prev_el;
     
-    size_t size = list->size;
-    realloc(list, (sizeof(linkedlist) * size) - 1);
+    set(*list, list->size, value);
 }
 
-#ifdef __cplusplus
-extern "C" 
-#endif
+void removeElement(linkedlist* list, const size_t element)
+{
+    struct LinkedListElement* el = getElement(*list, element);
+    
+    if(el == NULL || el == &NULL_LIST_ELEMENT) return;
+    
+    if(el->value) free(el->value);
+    
+    if(el != list->last) 
+    {
+        if(el == list->first) (el->next)->prev = &NULL_LIST_ELEMENT;
+        else (el->next)->prev = el->prev;
+    }
+    if(el != list->first) (el->prev)->next = el->next;
+    
+    for(size_t i = element; element < (list->size - element); i++) el = el->next;
+    
+    free(el);
+    
+    --list->size;
+}
+
 void destroy(linkedlist* list)
 {
-    struct LinkedListElement* el = list.first;
-    size_t size = list->size;
+    struct LinkedListElement* el = list->first;
     
-    for(size_t i = 0; i < size; i++) 
+    for(size_t i = 0; i < list->size; i++) 
     {
-        linkedlist* listptr = el;
+        struct LinkedListElement* listptr = el;
         el = el->next;
         
-        free(el);
+        free(listptr);
     }
     
     free(list);
 }
+
+#ifdef __cplusplus 
+}
+#endif
 
 #endif
